c158a1d19f87cde12664fa2fbec34055b56e2202
Saeed <najafisaeed@gmail.com>
2021-07-31 17:19:35 +0200

    video

--- radix/src/components/Navbar/NavbarElements.js
+++ radix/src/components/Navbar/NavbarElements.js
@@ -6,7 +6,7 @@ import { FaBars } from "react-icons/fa";
 export const Nav = styled.nav`
   background: #000;
   height: 80px;
-  // margin-top: -80px;
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-item: center;
--- radix/src/components/pages/Home.js
+++ radix/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../Navbar";
 import Sidebar from "../SideBar";
+import HeroSection from "../HeroSection";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,54 +12,57 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100px",
-          backgroundColor: "red",
-          // top: 100,
-        }}
-      >
-        <h1>Home</h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100px",
-          backgroundColor: "red",
-          // top: 100,
-        }}
-      >
-        <h1>Home</h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "400px",
-          backgroundColor: "red",
-          // top: 100,
-        }}
-      >
-        <h1>Home</h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "500px",
-          backgroundColor: "red",
-          // top: 100,
-        }}
-      >
-        <h1>Home</h1>
-      </div>
+      <HeroSection />
+      {/* <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100px",
+            backgroundColor: "red",
+            // top: 100,
+          }}
+        >
+          <h1>Home</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100px",
+            backgroundColor: "red",
+            // top: 100,
+          }}
+        >
+          <h1>Home</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "400px",
+            backgroundColor: "red",
+            // top: 100,
+          }}
+        >
+          <h1>Home</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "500px",
+            backgroundColor: "red",
+            // top: 100,
+          }}
+        >
+          <h1>Home</h1>
+        </div>
+      </div> */}
     </>
   );
 };
